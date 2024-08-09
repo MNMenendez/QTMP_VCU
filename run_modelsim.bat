a741f37a1f20ff629cfed003a19719e6d2febaa5
MNMenendez <118775108+MNMenendez@users.noreply.github.com>
2024-08-09 11:02:25 +1000

    Delete run_modelsim.bat

--- run_modelsim.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Running ModelSim
-vlib work
-vcom -2008 -work work "%WORKSPACE%\\QTMP_VCU\\source\\*.vhdl"
-vcom -2008 -work work "%WORKSPACE%\\QTMP_VCU\\testbenches\\*.vhdl"
-vsim -c -do "run -all; quit"
